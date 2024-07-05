117aa4d52c5bf9ce39820b7a0c3f3533067dab8a
nbouvier-inria <nathan.bouvier@inria.fr>
2024-07-05 10:13:39 +0200

    Added comments

--- nevAI/nevAInotarget.py
+++ nevAI/nevAInotarget.py
@@ -84,8 +84,11 @@ def bit_flips(tensor1, tensor2):
 
 
 def voltage_modify(x, T):
-        x = 2*x/T
-        return torch.where(x > 1, x - 1, 1-1/x)
+        """
+        Modification of x to widen or recenter the output
+        """
+        # x = 2*x/T
+        return x #torch.where(x > 1, x - 1, 1-1/x)
 
 
 def mutate_tensor(tensor, k):
@@ -105,8 +108,8 @@ def mutate_tensor(tensor, k):
     indices = torch.randperm(num_elements)[:k]
     result = tensor.clone()
     for idx in indices:
-        # Randomly select a value to flip to (-1, 0, or 1)
-        value = torch.randint(0, 2, (1,)).item()
+        # Randomly select a value to flip to (-1, or 1)
+        value = 2*torch.randint(0, 2, (1,)).item()-1
         result.view(-1)[idx] = value
     return result
 
@@ -159,17 +162,17 @@ class NonSpikingLIFNode(neuron.LIFNode):
 
 # Spiking DQN algorithm
 class DQSN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, hidden_layers=1, T=16):
+    def __init__(self, input_size, hidden_size, output_size, hidden_layers=1, T=16, Vth=1.):
         super().__init__()
 
         layers = OrderedDict()
         layers["0th_layer"] = layer.Linear(input_size, hidden_size)
-        layers["0th_IF"] = neuron.IFNode()
+        layers["0th_IF"] = neuron.IFNode(v_threshold=Vth)
         for i in range(hidden_layers):
             layers[f"{i+1}th_laye"] = layer.Linear(hidden_size, hidden_size)
-            layers[f"{i+1}th_IF"] = neuron.IFNode()
+            layers[f"{i+1}th_IF"] = neuron.IFNode(v_threshold=Vth)
         layers[f"{hidden_layers+1}th_layer"] = layer.Linear(hidden_size, output_size)
-        layers[f"output_layer"] = NonSpikingLIFNode(tau=2.0)
+        layers[f"output_layer"] = NonSpikingLIFNode(tau=1.01)
 
         self.fc = nn.Sequential(
             layers
@@ -180,34 +183,40 @@ class DQSN(nn.Module):
     def forward(self, x):
         for t in range(self.T):
             self.fc(x)
-            
         return voltage_modify(self.fc[-1].v, self.T)
 
 
-def train(use_cuda, model_dir, log_dir, env_name, hidden_size, num_episodes, seed):
-    N = 16
-    BATCH_SIZE = 128
+def train(use_cuda, model_dir, log_dir, env_name, num_episodes, seed, hidden_size=128):
+    N = 49
+    BATCH_SIZE = 256
     REPLAY_SIZE = 10000 * N
     P_COMBINE = 0.5
     P_MUTATE = 0.5
     
-    GAMMA_START = 0.2
-    GAMMA_END = 0.999
-    GAMMA_DECAY = 2000
-    TARGET_UPDATE = 1
+    # General simulation control
+    EPS_START = 0.9
+    EPS_END = 0.05
+    EPS_DECAY = 200
+
+    # Discount factor control
+    EPSILON = 0.001
+    GAMMA_START = 0
+    GAMMA_END = 1 - EPSILON
+    GAMMA_DECAY = 50
     HIDDEN_LAYERS = 5
-    
+    VTH = 4.
+    FINAL_REWARD = -10
     v, e = torus(N)
     NEIGHBOURS = graph_to_N(e, v)
 
-    MUTATION_STRENGTH = 1/100
+    MUTATION_STRENGTH = 1/1000
     T = 16
 
     def set_binary():
         lengths = [[policy_net[individual].fc[2*i].weight.flatten() for i in range(HIDDEN_LAYERS+2)] for individual in range(N)]
         weights = [torch.cat(length) for length in lengths]
         dimension = weights[0].shape
-        weights = [torch.randint(0, 2, dimension).float().to(device) for _ in weights]
+        weights = [2*torch.randint(0, 2, dimension).float().to(device)-1 for _ in weights]
         
         for individual in range(N):
         # Update the weights to the new computed values if a change have been made
@@ -230,64 +239,68 @@ def train(use_cuda, model_dir, log_dir, env_name, hidden_size, num_episodes, see
 
     steps_done = 0
 
-    env = [gym.make(env_name, render_mode="rgb_array").unwrapped for _ in range(N)]
+    env = gym.make(env_name, render_mode="rgb_array").unwrapped
 
-    n_states = env[0].observation_space.shape[0]
-    n_actions = env[0].action_space.n
+    n_states = env.observation_space.shape[0]
+    n_actions = env.action_space.n
 
-    # Create spiking neural networks
-    policy_net: List[DQSN]= [DQSN(input_size=n_states, hidden_size=hidden_size, output_size=n_actions, hidden_layers=HIDDEN_LAYERS, T=T).to(device) for _ in range(N)]
-    target_net: DQSN = DQSN(input_size=n_states, hidden_size=hidden_size, output_size=n_actions, hidden_layers=HIDDEN_LAYERS, T=T).to(device) # for _ in range(N)]
+    # Create spiking neural networks[round(float(i), 2)  for i in loss if i is not float("inf")]
+    policy_net: List[DQSN]= [DQSN(input_size=n_states, hidden_size=hidden_size, output_size=n_actions, hidden_layers=HIDDEN_LAYERS, T=T, Vth=VTH).to(device) for _ in range(N)]
     set_binary()
-    target_net.load_state_dict(policy_net[0].state_dict()) # (target_net[i].load_state_dict(policy_net[i].state_dict()) for i in range(N))
-    target_net.eval()
 
     # Initialize replay memory
     memory = ReplayMemory(REPLAY_SIZE)
 
     @torch.no_grad
     def select_action(state, steps_done, individual):
-        # sample = random.random()
-        # eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-        #                 math.exp(-1. * steps_done / EPS_DECAY)
-        # if sample > eps_threshold:
+        sample = random.random()
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+                        math.exp(-1. * steps_done / EPS_DECAY)
+        if sample > eps_threshold:
             ac = policy_net[individual](state).max(1).indices
             ac = ac.view(1, 1)
             functional.reset_net(policy_net[individual])
             return ac
-        # else:
-        #     return torch.tensor([[random.randrange(env[individual].action_space.n)]], device=device, dtype=torch.long)
+        else:
+            return torch.tensor([[random.randrange(env.action_space.n)]], device=device, dtype=torch.long)
 
     @torch.no_grad
-    def get_loss(individual, transitions, time_step):
+    def get_loss(policy, transitions, time_step):
 
         if len(memory) < BATCH_SIZE:
             return
         transitions = memory.sample(BATCH_SIZE)
         batch = Transition(*zip(*transitions))
 
+        # non_final_mask[state] if and only if state is non final
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
                                                 batch.next_state)), device=device, dtype=torch.bool)
+        # List of non final states
         non_final_next_states = torch.cat([s for s in batch.next_state
                                                     if s is not None])
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
 
-        state_action_values = policy_net[individual](state_batch).gather(1, action_batch)
+        # Compute the Q-Value
+        state_action_values = policy(state_batch).gather(1, action_batch)
+        functional.reset_net(policy)
 
+        # Compute maximum Q-Value of future states
         next_state_values = torch.zeros(BATCH_SIZE, device=device)
-        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
-        functional.reset_net(target_net)
+        next_state_values[non_final_mask] = policy(non_final_next_states).max(1)[0].detach()
+        functional.reset_net(policy)
 
         # Compute the expected Q-Value
-        expected_state_action_values = (next_state_values *  (GAMMA_END + (GAMMA_START - GAMMA_END) * \
-                        math.exp(-1. * time_step/ GAMMA_DECAY)) ) + reward_batch
+        gamma = (GAMMA_END + (GAMMA_START - GAMMA_END) * \
+                        math.exp(-1. * time_step/ GAMMA_DECAY))
+        expected_state_action_values = (next_state_values * gamma) + torch.where(non_final_mask, reward_batch, FINAL_REWARD)
 
-        # print(float(state_action_values), float(expected_state_action_values))
+        # Print information on some Action - Reward computiations
+        if individual == 0 :
+           print([round(float(j), 1) for j in expected_state_action_values[0:10]], [round(float(i),1) for i in state_action_values[0:10]])
         # Get the loss
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-        functional.reset_net(policy_net[individual])
         return loss
     
     @torch.no_grad
@@ -305,9 +318,9 @@ def train(use_cuda, model_dir, log_dir, env_name, hidden_size, num_episodes, see
             better = []
             if loss[individual] is not None:
                 for n in NEIGHBOURS[individual]:
-                    if loss[n] is not None and loss[n] < loss[individual] and np.random.random() < P_COMBINE:
+                    if loss[n] is not None and loss[n] < loss[individual]:
                         better.append(n)
-            if better != []:
+            if better != [] and np.random.random() < P_COMBINE:
                 mate = np.random.choice(better)
                 bbefore = bit_flips(weights[mate], weights[individual])
                 new_weights.append(combine_tensors(weights[mate], weights[individual]))
@@ -342,72 +355,67 @@ def train(use_cuda, model_dir, log_dir, env_name, hidden_size, num_episodes, see
     for i_episode in range(num_episodes):
 
         # Initialize the environment and state
-        [env.reset() for env in env]
-        state = [torch.zeros(size=[1, n_states], dtype=torch.float, device=device) for _ in range(N)]
-        total_reward = [0 for _ in range(N)]
+        env.reset()
+        state = torch.zeros(size=[1, n_states], dtype=torch.float, device=device)
+        total_reward = 0
         loss = [10 for _ in range(N)]
-        done = [False for _ in range(N)]
+        done = False
 
         ended = 0
 
-        while ended < N-1:
+        while not done:
 
             steps_done += 1
 
-            for individual in range(N):
+            # Choose the best action given current policy_net
+            action = select_action(state, i_episode, np.random.randint(N))
+            
+            # Get the next state and reward
+            next_state, reward, termination, truncation,_ = env.step(action.item())
+            done = termination or truncation
+            total_reward += reward
+            next_state = torch.from_numpy(next_state).float().to(device).unsqueeze(0)
+            reward = torch.tensor([reward], device=device)
 
-                if not done[individual]:
-                    # Choose the best action given current policy_net
-                    action = select_action(state[individual], i_episode, individual)
-                    
-                    # Get the next state and reward
-                    next_state, reward, termination, truncation,_ = env[individual].step(action.item())
-                    done[individual] = termination or truncation
-                    total_reward[individual] += reward
-                    next_state = torch.from_numpy(next_state).float().to(device).unsqueeze(0)
-                    reward = torch.tensor([reward], device=device)
+            if done:
+                next_state = None
+                # ended += 1
 
-                    if done[individual]:
-                        next_state = None
-                        ended += 1
+            # Save the current state in replay memory
+            current_state = Transition(state, action, next_state, reward)
+            memory.push(state, action, next_state, reward)
 
-                    # Save the current state in replay memory
-                    current_state = Transition(state[individual], action, next_state, reward)
-                    memory.push(state[individual], action, next_state, reward)
-
-                    state[individual] = next_state
-                    if done[individual] and total_reward[individual] > max_reward:
-                        print(f"Individual {individual} has improved the best solution")
-                        max_reward = total_reward[individual]
-                        torch.save(policy_net[individual].state_dict(), max_pt_path)
-                    
+            state = next_state
+            if done and total_reward > max_reward:
+                # print(f"Individual {individual} has improved the best solution")
+                max_reward = total_reward
+                torch.save(policy_net[0].state_dict(), max_pt_path)
+            
 
                     # Compute the loss function
-                    if not done[individual]:
-                        loss[individual] = get_loss(individual, [current_state], steps_done) # get_loss(individual, steps_done)
-                    else:
-                        loss[individual] = 1000 - total_reward[individual]
-            
+            if not done:
+                for individual in range(N):
+                    loss[individual] = get_loss(policy_net[individual], [current_state], steps_done) # get_loss(individual, steps_done)
+
             if not None in loss:
-                print("Losses are :", round(sum([round(float(i), 2)  for i in loss]), 2))
+                ponderate = [round(float(i), 2)  for i in loss if i != float("inf")]
+                if ponderate != []:
+                    print(f"Losses at time step {steps_done} are :", round(sum(ponderate)*100/len(ponderate), 2))
 
             # Choose better policy nets
             update_weights(loss)
 
-        print(f'Episode {i_episode} with a sum of rewards {sum(total_reward)} for gamma={round((GAMMA_END + (GAMMA_START - GAMMA_END) * math.exp(-1. * steps_done/ GAMMA_DECAY)), 3)}')
-        print(f'Total rewards are: {total_reward}')
-
-        if i_episode % TARGET_UPDATE == 0:
-            # print("Target:", target_net.fc[0].weight[:3])
-            # print("Policy:", policy_net[0].fc[0].weight[:3])
-            print(f"Best current policy is {np.argmax(total_reward)} with a reward value of {np.max(total_reward)}")
-            target_net.load_state_dict(torch.load(max_pt_path)) # policy_net[np.argmax(total_reward)].state_dict())
+        print(f'Episode {i_episode} with a reward of {round(np.average(total_reward), 1)} for gamma = {round((GAMMA_END + (GAMMA_START - GAMMA_END) * math.exp(-1. * steps_done/ GAMMA_DECAY)), 3)}')
+        # print(f'Total rewards are: {total_reward}')
+        # print("Target:", target_net.fc[0].weight[:3])
+        # print("Policy:", policy_net[0].fc[0].weight[:3])
+        # print(f"Best current policy is {np.argmax(total_reward)} with a reward value of {np.max(total_reward)}")
 
     print('complete')
-    torch.save(policy_net.state_dict(), pt_path)
+    torch.save(policy_net[0].state_dict(), pt_path)
     print('state_dict path is', pt_path)
 
 game = "CartPole-v1"
 
 train(use_cuda=True, model_dir=f'./model/{game}', log_dir='./log', env_name=game, \
-        hidden_size=1024, num_episodes=50, seed=1)
+        num_episodes=50, seed=1)
